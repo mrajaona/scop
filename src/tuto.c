8325fa69bd29969e3cab5c33254ade6099f711ad
maryse <rajaon_m@naver.com>
2019-05-01 10:16:14 +0200

    errlogs

--- src/tuto.c
+++ src/tuto.c
@@ -31,8 +31,8 @@ int			main()
 		return (1);
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 

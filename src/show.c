ade51f7b1ef38036ca244f98fd08362b98a0fc6f
Maryse RAJAONARY <mrajaona@e2r11p8.42.fr>
2019-05-07 10:00:00 +0200

    ou alors le brew uptade a repare

--- src/show.c
+++ src/show.c
@@ -1,11 +1,9 @@
 #include "show.h"
-#include <unistd.h> // debug
 
 void	show(t_data *scop)
 {
 	while(!glfwWindowShouldClose(scop->window))
 	{
-		sleep(1); // debug
 		glfwSwapBuffers(scop->window);
 		glfwPollEvents();
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);

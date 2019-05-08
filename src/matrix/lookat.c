564026a6e4dbc153bfd8a5b8a9539e357cd12f66
maryse <rajaon_m@naver.com>
2019-05-08 13:44:18 +0200

    je sais même pas ce que je suis censée voir, en fait

--- src/matrix/lookat.c
+++ src/matrix/lookat.c
@@ -29,13 +29,32 @@ void			lookat(const t_vector eye, const t_vector target,
 	coord_cross_prod(cam_direction, tmp, cam_right);
 
 	coord_cross_prod(cam_direction, cam_right, cam_up);
-	vector_scalar(cam_up, -1, cam_up); // fix foireux
 
 	coord_normalize(cam_direction, d);
 	coord_normalize(cam_up, u);
 	coord_normalize(cam_right, r);
 
 	identity(lhs);
+
+	mat4_set(lhs, 0, 0, r[0]);
+	mat4_set(lhs, 1, 0, r[1]);
+	mat4_set(lhs, 2, 0, r[2]);
+
+	mat4_set(lhs, 0, 1, u[0]);
+	mat4_set(lhs, 1, 1, u[1]);
+	mat4_set(lhs, 2, 1, u[2]);
+
+	mat4_set(lhs, 0, 2, d[0]);
+	mat4_set(lhs, 1, 2, d[1]);
+	mat4_set(lhs, 2, 2, d[2]);
+
+	t_mat4	rhs;
+
+	identity(rhs);
+
+	mat4_set(rhs, 3, 0, -1 * eye[0]);
+	mat4_set(rhs, 3, 1, -1 * eye[1]);
+	mat4_set(rhs, 3, 2, -1 * eye[2]);
 
 	mat4_mult(lhs, rhs, dest);
 }

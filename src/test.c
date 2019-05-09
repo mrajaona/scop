9d6aaa47770bc04a25f7e59632d21604abba9c4a
maryse <rajaon_m@naver.com>
2019-05-09 09:43:02 +0200

    ca a l'air bien

--- src/test.c
+++ src/test.c
@@ -9,6 +9,38 @@
 #include "angle.h"
 #include "error.h"
 
+static void	mat4_print(t_mat4 mat)
+{
+	fprintf(stdout, "\n");
+	fprintf(stdout, "% .2f % .2f % .2f % .2f\n",
+		mat4_get(mat, 0, 0),
+		mat4_get(mat, 1, 0),
+		mat4_get(mat, 2, 0),
+		mat4_get(mat, 3, 0)
+		);
+	fprintf(stdout, "% .2f % .2f % .2f % .2f\n",
+		mat4_get(mat, 0, 1),
+		mat4_get(mat, 1, 1),
+		mat4_get(mat, 2, 1),
+		mat4_get(mat, 3, 1)
+		);
+	fprintf(stdout, "% .2f % .2f % .2f % .2f\n",
+		mat4_get(mat, 0, 2),
+		mat4_get(mat, 1, 2),
+		mat4_get(mat, 2, 2),
+		mat4_get(mat, 3, 2)
+		);
+	fprintf(stdout, "% .2f % .2f % .2f % .2f\n",
+		mat4_get(mat, 0, 3),
+		mat4_get(mat, 1, 3),
+		mat4_get(mat, 2, 3),
+		mat4_get(mat, 3, 3)
+		);
+	fprintf(stdout, "\n");
+
+	fflush(stdout);	
+}
+
 static void set_model(t_data *scop)
 {
 	t_mat4		model;
@@ -16,11 +48,14 @@ static void set_model(t_data *scop)
 	identity(model);
 
 	// t_vector	edit;
-	// coord_to_vec(1.0f, 1.0f, 0.0f, edit);	
-	// scaling(edit, model);
-	//translation(edit, model);
+	// coord_to_vec(.0f, 1.0f, 0.0f, edit);	
+	// // scaling(edit, model);
+
+	// translation(edit, model);
 
 	// rotatez(deg_to_rad(180.0f), model);
+
+	mat4_print(model);
 
 	GLint uniTrans = glGetUniformLocation(scop->shaderProgram, "model");
 	glUniformMatrix4fv(uniTrans, 1, GL_FALSE, model);
@@ -45,13 +80,14 @@ static void	set_view(t_data *scop)
 
 	// identity
 	// coord_to_vec(0, 0, 0, eye);
-	// coord_to_vec(0, 0, 1, target);
-	// coord_to_vec(0, -1, 0, up);
+	// coord_to_vec(0, 0, -1, target);
+	// coord_to_vec(0, 1, 0, up);
 
 	coord_to_vec(0, 0, 0, eye);
-	coord_to_vec(0, 0, 1, target);
-	coord_to_vec(0, -1, 0, up);
+	coord_to_vec(0, 0, -1, target);
+	coord_to_vec(0, 1, 0, up);
 
+	mat4_print(view);
 	// lookat(eye, target, up, view);
 
 	GLint uniView = glGetUniformLocation(scop->shaderProgram, "view");
@@ -80,6 +116,7 @@ static void	set_proj(t_data *scop)
 	// // planes[NEAR_PLANE] = 1.0f;
 	// // planes[FAR_PLANE] = 10.0f;
 
+	mat4_print(proj);
 	// perspective(vfov, ratio, planes, proj);
 
 	GLint uniProj = glGetUniformLocation(scop->shaderProgram, "proj");

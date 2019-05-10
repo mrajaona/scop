8d59ee31e3a85d37ff2167f4977e13988dc8b1f0
maryse <rajaon_m@naver.com>
2019-05-10 12:07:41 +0200

    tourne

--- inc/show.h
+++ inc/show.h
@@ -1,7 +1,9 @@
 #ifndef SHOW_H
 # define SHOW_H
 
+# include "gl_loader.h"
 # include "common.h"
+# include "matrix_rotations.h"
 
 void	show(const t_data *scop);
 
--- src/show.c
+++ src/show.c
@@ -1,14 +1,32 @@
 #include "show.h"
 
+static void	edit_output(const t_data *scop)
+{
+	t_mat4			model;
+	static int		deg = 0;
+
+	identity(model);
+	deg = (deg + 10) % 360;
+	rotatez(deg_to_rad((float)deg), model);
+
+    GLint uniTrans = glGetUniformLocation(scop->shaderProgram, "model");
+	glUniformMatrix4fv(uniTrans, 1, GL_FALSE, model);
+}
+
+#include <unistd.h>
 void	show(const t_data *scop)
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f );
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	while(!glfwWindowShouldClose(scop->window))
 	{
+		sleep(1);
+		edit_output(scop);
+
 		glfwSwapBuffers(scop->window);
 		glfwPollEvents();
+		
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f );
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 	}
 }

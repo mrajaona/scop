6c821028beae0fdbadb39fa59788562756279d94
Maryse RAJAONARY <mrajaona@e3r3p8.42.fr>
2019-05-24 13:05:57 +0200

    merge

--- inc/img_format.h
+++
@@ -1,7 +0,0 @@
-#ifndef IMG_FORMAT_H
-# define IMG_FORMAT_H
-
-# define BYTES_PER_PIXEL 3
-# define TEX_FORMAT GL_RGB
-
-#endif
--- inc/show.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHOW_H
-# define SHOW_H
-
-# include "common.h"
-
-void	show(t_data *scop);
-
-#endif

7f9370824bf693fb74331c299df3a396944e2217
maryse <rajaon_m@naver.com>
2019-05-31 13:55:15 +0200

    notes

--- rendu/inc/more/model_struct.h
+++ rendu/inc/more/model_struct.h
@@ -6,6 +6,30 @@
 
 # include "vector.h"
 
+/*
+** name		: material name
+** Ns		: specular exponent
+** Ka		: ambient color
+** Kd		: diffuse color
+** Ks		: specular color
+** Ni		: optical_density (index of refraction)
+** d		: dissolve (opacity)
+** ** Tr	: transparency = 1 - d
+** illum	: illumination model
+** ** 0. Color on and Ambient off
+** ** 1. Color on and Ambient on
+** ** 2. Highlight on
+** ** 3. Reflection on and Ray trace on
+** ** 4. Transparency: Glass on, Reflection: Ray trace on
+** ** 5. Reflection: Fresnel on and Ray trace on
+** ** 6. Transparency: Refraction on, Reflection: Fresnel off and Ray trace on
+** ** 7. Transparency: Refraction on, Reflection: Fresnel on and Ray trace on
+** ** 8. Reflection on and Ray trace off
+** ** 9. Transparency: Glass on, Reflection: Ray trace off
+** ** 10. Casts shadows onto invisible surfaces
+*/
+
+
 typedef struct	s_material
 {
 	char		*name;
